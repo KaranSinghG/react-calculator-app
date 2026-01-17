b4aa87c1ba36ed3d329d5bc741e3e4324329cb6f
KaranSinghG <karansinghg@ymail.com>
2026-01-17 21:50:49 +0530

    initialized electron

---
+++ electron/main.js
@@ -0,0 +1,18 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+  });
+
+  win.loadURL("http://localhost:3000");
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
